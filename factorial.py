0dd201c621df4eea54b302fcb2342187e03286ab
alexgriego <71595087+alexgriego@users.noreply.github.com>
2020-09-27 09:54:58 -0500

    Delete factorial.py

--- factorial.py
+++
@@ -1,7 +0,0 @@
-n = int (input("Digite numero: "))
-def factorial(n):
-    if (n==0):
-        return 1
-    else:
-        return n * factorial(n-1)
-print("El factorial de : " + str(n) + ": es: " + str(factorial(n)))
